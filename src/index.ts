2c77fa9add2a083389c5f65f6224f1837541709b
MohammedNiyasK <niyask1997@gmail.com>
2023-04-13 19:58:39 +0530

    change in express

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const app = express()
 const port = 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors())
 app.use("/auth", authRouter);
